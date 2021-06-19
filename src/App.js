eb48b03f8388a5ae4ac248fc27e5b61cf0a93df0
Shivam Mishra <mshiv619@gmail.com>
2021-06-19 06:40:30 +0530

    docs: updated database

--- src/App.js
+++ src/App.js
@@ -5,9 +5,54 @@ var emojiDictionary = {
   "😊": "smiling",
   "😳": "disbelief",
   "😔": "sad",
-  "🥡": "takeout box",
-  "❤️": "love",
-  "😑": "annoyance"
+  "😑": "annoyance",
+  "🤤": "Drooling Face",
+  "😴": "Sleeping Face",
+  "😷": "Face with Medical Mask",
+  "🤣": "Rolling on the Floor Laughing",
+  "🙃": "Upside-Down Face",
+  "😉": "Winking Face",
+  "😍": "Smiling Face with Heart-Eyes",
+  "🤩": "Star-Struck",
+  "😘": "Face Blowing a Kiss",
+  "😋": "Face Savoring Food",
+  "🤑": "Money-Mouth Face",
+  "🤫": "Shushing Face",
+  "🥵": "Hot Face",
+  "🥶": "Cold Face",
+  "🤐": "Zipper-Mouth Face",
+  "😏": "Smirking Face",
+  "🥺": "Pleading Face",
+  "😒": "Unamused Face",
+  "🙄": "Face with Rolling Eyes",
+  "😎": "Smiling Face with Sunglasses",
+  "😭": "Loudly Crying Face",
+  "😱": "Face Screaming in Fear",
+  "😡": "Pouting Face",
+  "😠": "Angry Face",
+  "😀": "Grinning Face",
+  "😃": "Grinning Face with Big Eyes",
+  "😄": "Grinning Face with Smiling Eyes",
+  "😅": "Grinning Face with Sweat",
+  "🤪": "Zany Face",
+  "🤗": "Hugging Face",
+  "🤨": "Face with Raised Eyebrow",
+  "😐": "Neutral Face",
+  "🤥": "Lying Face",
+  "😌": "Relieved Face",
+  "😔": "Pensive Face",
+  "😪": "Sleepy Face",
+  "🤒": "Face with Thermometer",
+  "🤕": "Face with Head-Bandage",
+  "🤢": "Nauseated Face",
+  "🤮": "Face Vomiting",
+  "🤧": "Sneezing Face",
+  "🥳": "Partying Face",
+  "😰": "Anxious Face with Sweat",
+  "🥱": "Yawning Face",
+  "😤": "Face with Steam From Nose",
+  "😈": "Smiling Face with Horns",
+  "👿": "Angry Face with Horns",
 };
 
 var emojisWeKnow = Object.keys(emojiDictionary);
@@ -20,7 +65,7 @@ export default function App() {
     var meaning = emojiDictionary[userInput];
 
     if (meaning === undefined) {
-      meaning = "Sorry, this is not in out database";
+      meaning = <h3>"Sorry, this is not in out database"</h3>;
     }
     setMeaning(meaning);
   }
@@ -32,17 +77,16 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1> Emoji </h1>
-      <input onChange={emojiInputHandler} placeholder={"Search your emoji"} />
+      <h1> Emoji Interpreter</h1>
+      <input onChange={emojiInputHandler} placeholder={"Search your emoji here"} />
 
       <h2> {meaning} </h2>
-
-      <h3> emojis we know </h3>
+      <h3 > Emojis We Know Currently 👇</h3>
       {emojisWeKnow.map(function (emoji) {
         return (
           <span
             onClick={() => emojiClickHandler(emoji)}
-            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer"}}
             key={emoji}
           >
             {emoji}
